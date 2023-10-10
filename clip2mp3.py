03de852bcbdae4bf2092c4a0e4df6f03e8136a32
Wheaties466 <Mwheat42289@gmail.com>
2023-10-10 11:41:48 -0400

    Update clip2mp3.py

--- clip2mp3.py
+++ clip2mp3.py
@@ -9,15 +9,13 @@ from pydub import AudioSegment
 TEMP_VIDEO_FILENAME = "temp_video.mp4"
 
 def is_valid_twitch_url(url):
-    # Check if it's a Twitch clip URL format (this can change based on Twitch's URL scheme)
     pattern = re.compile(r'^https://clips\.twitch\.tv/[A-Za-z0-9]+')
     return bool(pattern.match(url))
 
 def is_video_file(filename):
-    # Check file magic bytes for common video formats
     video_magic_bytes = [
         b'\x00\x00\x00 ftypisom',  # MP4
-        b'\x1aE\xdf\xa3'           # MKV (WebM also has this signature but different segments)
+        b'\x1aE\xdf\xa3'           # MKV
     ]
     with open(filename, 'rb') as file:
         file_start = file.read(16)
@@ -28,17 +26,17 @@ def is_video_file(filename):
 
 def download_clip(url):
     ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',  # Adjusted this line to select the best available format
+        'format': 'bestvideo+bestaudio/best',
         'outtmpl': TEMP_VIDEO_FILENAME,
         'postprocessors': [],
-        #'logger': MyLogger(),#
-        #'progress_hooks': [hook],
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        info_dict = ydl.extract_info(url, download=True)
+        title = info_dict.get('title', None)
+        return title  # Return the video title
 
-def convert_to_mp3(video_filename, audio_filename="output.mp3"):
+def convert_to_mp3(video_filename, audio_filename):
     clip = AudioFileClip(video_filename)
     clip.write_audiofile(audio_filename)
 
@@ -53,12 +51,18 @@ if __name__ == "__main__":
         print("Invalid Twitch clip URL.")
         sys.exit(1)
 
-    download_clip(url)
+    title = download_clip(url)
 
+    if not title:
+        print("Couldn't fetch the title for the clip.")
+        sys.exit(1)
+    
+    output_filename = f"{title}.mp3".replace(" ", "_")  # Replacing spaces with underscores for filename
+    
     if not is_video_file(TEMP_VIDEO_FILENAME):
         print("Downloaded file is not a valid video format.")
         os.remove(TEMP_VIDEO_FILENAME)
         sys.exit(1)
 
-    convert_to_mp3(TEMP_VIDEO_FILENAME)
-    print("Conversion done. Check output.mp3 for the audio!")
+    convert_to_mp3(TEMP_VIDEO_FILENAME, output_filename)
+    print(f"Conversion done. Check {output_filename} for the audio!")
